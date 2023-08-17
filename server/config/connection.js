c6bb03b75aecdf045c10c3ae79a653a512457c93
Trevor-McGuire <trevor.mcguire.candidate@gmail.com>
2023-08-17 14:16:55 -0500

    sequelize/mongoose

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,15 @@
+const Sequelize = require('sequelize');
 const { connect, connection } = require('mongoose');
+require('dotenv').config();
 
-const connectionString = process.env.JAWSDB_URL || 'mongodb://127.0.0.1:27017/taking-stockDB';
+let sequelize;
 
-connect(connectionString)
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/taking-stockDB';
+  connect(connectionString)
+}
 
 module.exports = connection;
+module.exports = sequelize;
