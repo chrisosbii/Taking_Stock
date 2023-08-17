40fc2ee0c990c414eb4b7397e1f11fcdbac9a8b3
Trevor-McGuire <trevor.mcguire.candidate@gmail.com>
2023-08-16 20:54:38 -0500

    home conditional render

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,6 +11,22 @@ export default function Home() {
   return (
     <><div className="card bg-white card-rounded w-50">
       <div>WELCOME HOME</div>
+      <div>
+          {Auth.loggedIn() ? (
+            <button className="btn btn-lg btn-light m-2" onClick={logout}>
+              Logout
+            </button>
+          ) : (
+            <>
+              <Link className="btn btn-lg btn-primary m-2" to="/login">
+                Login
+              </Link>
+              <Link className="btn btn-lg btn-light m-2" to="/signup">
+                Signup
+              </Link>
+            </>
+          )}
+        </div>
     </div><div className="card bg-white card-rounded w-50">
         <Link to="/Login">
           <button className="btn btn-lg btn-danger">Login</button>
