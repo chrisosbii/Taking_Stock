70fb7d8d57d07ecd00b3c1d74124caec3afb4af0
Trevor-McGuire <trevor.mcguire.candidate@gmail.com>
2023-08-17 14:21:10 -0500

    fixed connection.js export values

--- server/config/connection.js
+++ server/config/connection.js
@@ -6,10 +6,11 @@ let sequelize;
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
+  module.exports = sequelize;
 } else {
   const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/taking-stockDB';
   connect(connectionString)
+  module.exports = connection;
 }
 
-module.exports = connection;
-module.exports = sequelize;
+
