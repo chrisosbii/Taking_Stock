56da59de9f156044d95966f3284415f8280e523a
Chris Osborne <chrisosbii@gmail.com>
2023-08-17 18:16:54 -0500

    added stock profile

---
+++ client/src/components/StockProfile/index.jsx
@@ -0,0 +1,26 @@
+const FavoriteStockList = ({
+    stock
+  }) => {
+    if (!stock) {
+      return <h3>No Stock Selected</h3>;
+    }
+  
+    return (
+    
+      <div> 
+        <div key={stocks._id} className="card mb-3">
+            <div className="card-body bg-light p-2">
+            <p>Name: {stock.name}</p>
+            <p>Symbol: {stock.symbol}</p>
+            <p>Open: {stock.open}</p>
+            <p>Close: {stock.close}</p>
+            <p>High: {stock.High}</p>
+            <p>Low: {stock.close}</p>
+            <p>Volume: {stock.volume}</p>
+            </div>
+        </div>
+      </div>
+    );
+  };
+
+  export default StockProfile;
