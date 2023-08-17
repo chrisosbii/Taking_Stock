410a2eb510ea8c3c18087ff444124dd487e534a2
Chris Osborne <chrisosbii@gmail.com>
2023-08-16 22:43:47 -0500

    updated name of header file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,8 @@ import './App.css';
 import { Outlet } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/header.jsx';
+import Footer from './components/footer.jsx';
 
 const client = new ApolloClient({
   uri: '/graphql',
