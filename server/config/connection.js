358ea24141149804d20914516a1e65d8141745ed
Trevor-McGuire <trevor.mcguire.candidate@gmail.com>
2023-08-17 11:23:14 -0500

    added JAWSDB_URL to server config using raw string

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const { connect, connection } = require('mongoose');
 
-const connectionString = process.env.JAWSDB_URL || 'mongodb://127.0.0.1:27017/taking-stockDB';
+// const connectionString = process.env.JAWSDB_URL || 'mongodb://127.0.0.1:27017/taking-stockDB';
 
-connect(connectionString);
+connect('mysql://nz7mqvw26aq6140j:z6ta3gxuyk4sjlzt@acw2033ndw0at1t7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/nl9lv15hptv4ycwd');
 
 module.exports = connection;
