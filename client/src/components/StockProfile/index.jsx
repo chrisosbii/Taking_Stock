aca3fb60a438ca0a5a4e4d843c4c1acdb089ebc8
Chris Osborne <chrisosbii@gmail.com>
2023-08-17 19:32:01 -0500

    added details for stock profile

--- client/src/components/StockProfile/index.jsx
+++ client/src/components/StockProfile/index.jsx
@@ -1,6 +1,10 @@
+import { useState } from 'react';
+
 const StockProfile = ({
     stock
   }) => {
+    
+    const [expandedStock, setExpandedStock] = useState(null);
     if (!stock) {
       return <h3>No Stock Selected</h3>;
     }
@@ -17,6 +21,23 @@ const StockProfile = ({
             <p>High: {stock.High}</p>
             <p>Low: {stock.close}</p>
             <p>Volume: {stock.volume}</p>
+            <p>Previous Close: {stock.previous_close}</p>
+            <p>Change: {stock.change}</p>
+            <p>Percent Change: {stock.percent_change}</p>
+            <p>Average Volume: {stock.average_volume}</p>
+            <p>Is Market Open: {stock.is_market_open ? 'Yes' : 'No'}</p>
+            <h4>Fifty Two Week</h4>
+            
+                <ul>
+                    <li>Low: {stock.fifty_two_week.low}</li>
+                <li>High: {stock.fifty_two_week.high}</li>
+                <li>Low Change: {stock.fifty_two_week.low_change}</li>
+                <li>High Change: {stock.fifty_two_week.high_change}</li>
+                <li>Low Change Percent: {stock.fifty_two_week.low_change_percent}</li>
+                <li>High Change Percent: {stock.fifty_two_week.high_change_percent}</li>
+                <li>Range: {stock.fifty_two_week.range}</li>
+                </ul>
+                
             </div>
         </div>
       </div>
